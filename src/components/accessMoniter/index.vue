4964e0fa765fd88535cc25d4bc76607b71eee3c1
henry <249908834@qq.com>
2019-05-05 14:58:53 +0800

    fix index.vue

--- src/components/accessMoniter/index.vue
+++ src/components/accessMoniter/index.vue
@@ -54,87 +54,57 @@ export default {
   },
 };
 </script>
-<style scoped>
-#echarts {
-  height: 330px;
-
-  margin: 76px 0 0 0;
-}
-.charts {
-  clear: both;
-  font-weight: 500;
-  height: 540px;
-  background: rgba(255, 255, 255, 1);
-  box-shadow: 0px 2px 10px 0px rgba(211, 211, 211, 1);
-  border-radius: 5px;
-  margin-top: 0.32rem;
-}
-.round {
-  width: 2rem;
-  height: 0.68rem;
-  border: 1px solid rgba(203, 203, 203, 1);
-  border-radius: 0.34rem;
-  text-align: center;
-  vertical-align: middle;
-  line-height: 0.68rem;
-  margin: 0 auto;
-  margin-top: 0.74rem;
-  color: rgba(132, 132, 132, 1);
-}
-.box {
-  font-size: 0.28rem;
-  flex: 1;
-  text-align: center;
-  margin-top: 16px;
-}
-.number {
-  font-size: 1.2rem;
-  font-family: PingFang-SC-Bold;
-  /* font-weight:bold; */
-  color: rgba(39, 39, 39, 1);
-  line-height: 36px;
-}
-.box1 {
-  margin-left: 0 !important;
-}
-.dateSelect {
-  margin-top: 0.2rem;
-  font-size: 0.4rem;
-  font-family: "PingFang SC";
-  font-weight: 500;
-}
-.datepick {
-  margin-left: 140px;
-}
-.line {
-  background: rgba(203, 203, 203, 1);
-
-  border: 1px solid rgba(203, 203, 203, 1);
-  opacity: 0.5;
-  margin-top: 10px;
-}
-.btn {
-  box-shadow: 0px 5px 6px 0px rgba(179, 209, 241, 1);
-  margin-left: 13px;
-}
-.acount {
-  display: flex;
-  height: 220px;
-  margin-top: 16px;
-}
-.ac_contain {
-  flex: 1;
-  background-color: white;
-  margin-left: 17px;
-  box-shadow: 0px 2px 10px 0px rgba(211, 211, 211, 1);
-  border-radius: 5px;
-}
-p {
-  font-size: 0.4rem;
-  font-family: PingFang-SC;
-  font-weight: 500;
-
-  margin-left: 0.4rem;
-  color: rgba(39, 39, 39, 1);
+<style lang="scss" >
+#accessIndex {
+  .name {
+    font-size: 22px;
+    font-family: PingFang SC;
+    font-weight: bold;
+    color: rgba(77, 79, 92, 0.5);
+  }
+  .line {
+    margin: 0 auto;
+    width: 204px;
+    height: 0px;
+    border: 1px solid rgba(77, 79, 92, 0.1);
+  }
+  .el-main{
+    margin-left: 244px
+  }
+  .el-aside {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+     border-right: 1px solid rgba(77,79,92,0.2);
+    overflow:visible
+  }
+  .el-container {
+    position: relative;
+  }
+  .ivu-menu-light.ivu-menu-vertical
+    .ivu-menu-item-active:not(.ivu-menu-submenu):after {
+    display: none;
+  }
+  .el-header {
+    padding: 15px 30px;
+    height: 60px;
+    border-bottom: 1px solid rgba(77, 79, 92, 0.1);
+  }
+  .ivu-menu-item-selected {
+    background: #4c83ff;
+    color: white;
+  }
+  .ivu-menu-vertical .ivu-menu-item-group-title {
+    color: #4d4f5c;
+  }
+  .ivu-menu-item {
+    padding: 10px 24px;
+  }
+  .exit {
+    font-size: 14px;
+    font-family: PingFang SC;
+    color: rgba(77, 79, 92, 1);
+    float: right;
+  }
 }
 </style>
